033b860aea2444e16dc8de305ac0768a523a43da
haha143 <2293557957@qq.com>
2020-11-20 18:58:14 +0800

    POI和EasyExcel所有测试用例

---
+++ src/main/java/com/rang/easyExcel/TestEasyExcel.java
@@ -0,0 +1,80 @@
+package com.rang.easyExcel;
+
+import com.alibaba.excel.EasyExcel;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TestEasyExcel {
+    String PATH="D:/software/IDEA/projects/rang-poi/";
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    /**
+     * 最简单的写
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 直接写即可
+     */
+    @Test
+    public void simpleWrite() {
+        // 写法1
+        String fileName = PATH+ "easyexcel.xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+//        // 写法2
+//        fileName = PATH+ "easyexcel.xlsx";
+//        // 这里 需要指定写用哪个class去写
+//        ExcelWriter excelWriter = null;
+//        try {
+//            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+//            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+//            excelWriter.write(data(), writeSheet);
+//        } finally {
+//            // 千万别忘记finish 会帮忙关闭流
+//            if (excelWriter != null) {
+//                excelWriter.finish();
+//            }
+//        }
+    }
+
+    /**
+     * 最简单的读
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 由于默认一行行的读取excel，所以需要创建excel一行一行的回调监听器，参照{@link DemoDataListener}
+     * <p>3. 直接读即可
+     */
+    @Test
+    public void simpleRead() {
+        // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+        // 写法1：
+        String fileName = PATH+ "easyexcel.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+
+//        // 写法2：
+//        String fileName = PATH+ "easyexcel.xlsx";
+//        ExcelReader excelReader = null;
+//        try {
+//            excelReader = EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).build();
+//            ReadSheet readSheet = EasyExcel.readSheet(0).build();
+//            excelReader.read(readSheet);
+//        } finally {
+//            if (excelReader != null) {
+//                // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
+//                excelReader.finish();
+//            }
+//        }
+    }
+}
--- src/main/java/com/rang/poi/ExcelRead.java
+++ src/main/java/com/rang/poi/ExcelRead.java
@@ -1,16 +1,15 @@
 package com.rang.poi;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import java.io.FileInputStream;
 
 public class ExcelRead {
-    String PATH="D:/IDEA/projects/rang-poi/";
+    String PATH="D:/software/IDEA/projects/rang-poi/";
 
     @Test
     public void test03Read()throws Exception{
@@ -20,13 +19,25 @@ public class ExcelRead {
         Row row=sheet.getRow(0);
         Cell cell[]=new Cell[4];
         cell[0]=row.getCell(0);
-        cell[1]=row.getCell(1,Row.RETURN_NULL_AND_BLANK);
+        cell[1]=row.getCell(1);
         cell[2]=row.getCell(2);
         cell[3]=row.getCell(3);
         for(int i=0;i<4;i++){
             System.out.println(cell[i]);
         }
     }
+
+    @Test
+    public void test07ReadByPolicy()throws Exception{
+        FileInputStream fileInputStream=new FileInputStream(PATH+"test07BigData.xlsx");
+        Workbook workbook=new XSSFWorkbook(fileInputStream);
+        Sheet sheet=workbook.getSheetAt(0);
+        Row row=sheet.getRow(0);
+//        Cell cell=row.getCell(-1);
+        Cell cell=row.getCell(-1,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+        System.out.println(cell);
+    }
+
 
     @Test
     public void test07Read()throws Exception{
@@ -36,7 +47,7 @@ public class ExcelRead {
         Row row=sheet.getRow(0);
         Cell cell[]=new Cell[4];
         cell[0]=row.getCell(0);
-        cell[1]=row.getCell(1,Row.RETURN_NULL_AND_BLANK);
+        cell[1]=row.getCell(1);
         cell[2]=row.getCell(2);
         cell[3]=row.getCell(3);
         for(int i=0;i<4;i++){
@@ -46,18 +57,82 @@ public class ExcelRead {
 
     @Test
     public void testMultipleTypeRead()throws Exception{
-        FileInputStream fileInputStream=new FileInputStream(PATH+"test.xlsx");
-        Workbook workbook=new XSSFWorkbook(fileInputStream);
+        FileInputStream fileInputStream=new FileInputStream(PATH+"test.xls");
+        Workbook workbook=new HSSFWorkbook(fileInputStream);
         Sheet sheet=workbook.getSheetAt(0);
-        Row row=sheet.getRow(0);
-        Cell cell[]=new Cell[4];
-        cell[0]=row.getCell(0);
-        cell[1]=row.getCell(1,Row.RETURN_NULL_AND_BLANK);
-        cell[2]=row.getCell(2);
-        cell[3]=row.getCell(3);
-        for(int i=0;i<4;i++){
-            System.out.println(cell[i]);
+        Row rowTitle=sheet.getRow(0);
+        int cellNum=rowTitle.getLastCellNum();
+        if(rowTitle!=null){
+            for(int i=0;i<cellNum;i++){
+                Cell cell=rowTitle.getCell(i);
+                int cellType=cell.getCellType();
+                if(cell!=null){
+                    System.out.print(cell+"-"+cellType+" | ");
+                }
+            }
         }
+        System.out.println();
+        int RowNum=sheet.getLastRowNum();
+        for(int i=1;i<=RowNum;i++){
+            Row rowData=sheet.getRow(i);
+            if(rowData!=null){
+                int cellnum=rowData.getLastCellNum();
+                for(int j=0;j<cellnum;j++){
+                    Cell cell=rowData.getCell(j);
+                    int cellType=cell.getCellType();
+                    if(cell!=null){
+                        switch (cellType){
+                            case HSSFCell.CELL_TYPE_NUMERIC:
+                                System.out.print(cell.getNumericCellValue()+"-"+cellType+" | ");
+                                continue;
+                            case HSSFCell.CELL_TYPE_STRING:
+                                System.out.print(cell.getStringCellValue()+"-"+cellType+" | ");
+                                continue;
+                            case HSSFCell.CELL_TYPE_FORMULA:
+                                System.out.print("null"+"-"+cellType+" | ");
+                                continue;
+                            case HSSFCell.CELL_TYPE_BLANK:
+                                System.out.print(cell.getStringCellValue()+"-"+cellType+" | ");
+                                continue;
+                            case HSSFCell.CELL_TYPE_BOOLEAN:
+                                System.out.print(cell.getBooleanCellValue()+"-"+cellType+" | ");
+                                continue;
+                            case HSSFCell.CELL_TYPE_ERROR:
+                                System.out.print(cell.getErrorCellValue()+"-"+cellType+" | ");
+                                continue;
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+    @Test
+    public void testFORMULA()throws Exception{
+        FileInputStream fileInputStream=new FileInputStream(PATH+"test.xls");
+        Workbook workbook=new HSSFWorkbook(fileInputStream);
+        //获取到包含公式的单元格
+        Sheet sheet=workbook.getSheetAt(0);
+        Row row=sheet.getRow(3);
+        Cell cell=row.getCell(7);
+        //读取计算的公式
+        FormulaEvaluator formulaEvaluator=new HSSFFormulaEvaluator((HSSFWorkbook) workbook);
+        int cellType=cell.getCellType();
+        switch (cellType){
+            //单元格的类型是公式类型
+            case HSSFCell.CELL_TYPE_FORMULA:
+                //公式内容
+                String formula=cell.getCellFormula();
+                System.out.println(formula);
+                //执行公式之后,单元格内的值
+                CellValue evaluate=formulaEvaluator.evaluate(cell);
+                String cellValue=evaluate.formatAsString();
+                System.out.println(cellValue);
+                break;
+        }
+
     }
 
 
